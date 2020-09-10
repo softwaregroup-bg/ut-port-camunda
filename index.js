94c18efc8ff4fff16bd0eddd82da5070565b74e5
Evgeni Atanasov <genata93@gmail.com>
2020-09-10 21:19:25 +0300

    Feat/dfa (#1)
    * feat: objects as variables should be stringified
    
    * feat: pass params for get requests, parse stringified vars
    
    * feat: use mtid to determine state, request.receive to response.receive
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.0
    
    * feat: open task count api
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.1
    
    * feat: config swagger host and basePath
    
    * feat: allow configuration of api file props
    
    * style: fix typo
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.2
    
    * feat: task complete and external task complete with variables
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.3
    
    * feat: add params to swagger.json
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.4
    
    * feat: update get /task query params in swagger
    
    * [ci-skip][ci skip] version incremented to 7.0.4-dfa.5
    
    Co-authored-by: Evgeni Atanasov <evgeni.atanasov@softwaregroup-bg.com>
    Co-authored-by: Jenkins CI <jenkins.ci@softwaregroup-bg.com>

--- index.js
+++ index.js
@@ -7,38 +7,87 @@ module.exports = function camunda() {
                     json: true
                 },
                 openApi: {
-                    camunda: require.resolve('./swagger')
+                    camunda: [require.resolve('./swagger')]
+                },
+                mergeOptions: {
+                    mergeStrategies: {
+                        'openApi.camunda': 'combine'
+                    }
                 }
             };
         }
 
         handlers() {
             return {
-                receive: response => {
+                receive: (response, {mtid}) => {
+                    if (mtid === 'error') return response.body;
                     return response.payload;
                 },
-                send: params => {
-                    return {body: params};
-                },
+                send: params => ({
+                    ...params, body: params
+                }),
                 'camunda.task.fail.request.send': ({id, ...rest}) => ({
                     id,
                     parseResponse: false,
                     body: rest
                 }),
-                'camunda.task.complete.request.send': ({id, ...rest}) => ({
+                'camunda.externaltask.complete.request.send': ({id, ...rest}) => ({
                     id,
                     parseResponse: false,
                     body: rest
                 }),
-                'camunda.process.start.request.send': ({process, ...rest}) => ({
-                    key: process,
-                    body: {
-                        variables: Object.entries(rest).reduce((prev, [name, value]) => {
-                            prev[name] = typeof value === 'object' ? value : {value};
+                'camunda.process.start.request.send': ({process, ...rest}) => {
+                    const variables = Object.entries(rest).reduce((prev, [name, value]) => {
+                        prev[name] = typeof value === 'object' ? {value: JSON.stringify(value)} : {value};
+                        return prev;
+                    }, {});
+
+                    return {
+                        key: process,
+                        body: {
+                            variables
+                        }
+                    };
+                },
+                'camunda.variables.get.response.receive': (response, {mtid}) => {
+                    if (mtid === 'error') return response.body;
+                    return response.payload && Object.entries(response.payload).reduce((prev, [name, value]) => {
+                        try {
+                            const actualValue = JSON.parse(value.value);
+                            prev[name] = typeof actualValue === 'object' ? actualValue : value.value;
                             return prev;
-                        }, {})
-                    }
-                })
+                        } catch (e) {
+                            prev[name] = value.value;
+                            return prev;
+                        }
+                    }, {});
+                },
+                'camunda.task.claim.request.send': ({id, ...rest}) => ({
+                    id,
+                    parseResponse: false,
+                    body: rest
+                }),
+                'camunda.task.unclaim.request.send': ({id}) => ({
+                    id,
+                    parseResponse: false
+                }),
+                'camunda.task.complete.request.send': ({id, variables = {}}) => {
+                    const transformedVars = Object.entries(variables).reduce((prev, [key, value]) => {
+                        const variableType = typeof value;
+                        prev[key] = {
+                            value: variableType === 'object' ? JSON.stringify(value) : value,
+                            type: variableType === 'object' ? 'string' : variableType
+                        };
+                        return prev;
+                    }, {});
+                    return {
+                        id,
+                        parseResponse: false,
+                        body: {
+                            variables: transformedVars
+                        }
+                    };
+                }
             };
         }
     };
